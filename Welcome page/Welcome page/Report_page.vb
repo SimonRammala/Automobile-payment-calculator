64b6c05a6e55ece5d6491d7b4f0b67a749275aa4
Simon Rammala <simonrammala456@gmail.com>
2023-01-28 17:33:00 +0200

    Updated the programes Updated the pages

--- Welcome page/Welcome page/Report_page.vb
+++ Welcome page/Welcome page/Report_page.vb
@@ -10,13 +10,16 @@
         lblclients_cell_num.Text = Customer_Details.txtcell_num.Text
 
 
-
-
         lblemp_company_name.Text = Employment_details.txtcompany_name.Text
         lblemp_occ_at_company.Text = Employment_details.txtocupation.Text
         lblemp_yearly_salary.Text = Employment_details.txtsalary.Text
         lblemp_company_tell_num.Text = Employment_details.txtcompany_tell.Text
 
+
+        lblcar_brand.Text = Car_details.cmbcar_brand.Text
+        lblcar_name.Text = Car_details.cmbcar_name.Text
+        lblcar_body_type.Text = Car_details.cmbcar_body_type.Text
+        lblcar_price.Text = Car_details.txtcar_price.Text
 
 
     End Sub
